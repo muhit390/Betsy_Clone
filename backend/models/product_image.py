150b872fd15bfea7af1720c2a24c6518722e8545
Md Jawad Hossain Bhuiyan <muhit.bhuiyan@gmail.com>
2024-11-10 22:49:43 -0500

    *

--- backend/models/product_image.py
+++ backend/models/product_image.py
@@ -3,7 +3,7 @@ from app import db
 
 class ProductImage(db.Model):
     __tablename__ = 'product_images'
-    id = db.Column(db.BigInteger, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     product_id = db.Column(db.BigInteger, db.ForeignKey('products.id'), nullable=False)
     name = db.Column(db.String(255))
     preview_image = db.Column(db.Boolean, default=False)
