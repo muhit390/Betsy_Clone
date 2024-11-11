114f5cbf4081ae818cdda83ce737156b596402a7
Md Jawad Hossain Bhuiyan <muhit.bhuiyan@gmail.com>
2024-11-10 22:50:35 -0500

    *

--- backend/models/favorite.py
+++ backend/models/favorite.py
@@ -3,5 +3,5 @@ from app import db
 
 class Favorite(db.Model):
     __tablename__ = 'favorites'
-    user_id = db.Column(db.BigInteger, db.ForeignKey('users.id'), primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
     product_id = db.Column(db.BigInteger, db.ForeignKey('products.id'), primary_key=True)
