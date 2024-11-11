39a020ffbc3ca3f68c96ffb1e61c3f6fe2c9f924
Md Jawad Hossain Bhuiyan <muhit.bhuiyan@gmail.com>
2024-11-11 15:53:57 -0500

    after gitigonre

--- backend/config.py
+++ backend/config.py
@@ -2,4 +2,5 @@
 class Config:
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///etsy_clone.db'  # SQLite database file
+    SECRET_KEY = 'e39b073bc5f7d4f8a2bc1d879a8d06e1'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
