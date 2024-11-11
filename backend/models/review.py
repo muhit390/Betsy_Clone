01829984aaf90b60970fd411d38558e595f2f145
Md Jawad Hossain Bhuiyan <muhit.bhuiyan@gmail.com>
2024-11-10 22:49:02 -0500

    *

--- backend/models/review.py
+++ backend/models/review.py
@@ -2,7 +2,7 @@ from app import db
 
 class Review(db.Model):
     __tablename__ = 'reviews'
-    id = db.Column(db.BigInteger, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     product_id = db.Column(db.BigInteger, db.ForeignKey('products.id'), nullable=False)
     user_id = db.Column(db.BigInteger, db.ForeignKey('users.id'), nullable=False)
     review = db.Column(db.Text)
